8376d19c1c9a56a68f990442245586b9f44aa62e
Syrinx <alberto.pesce98@gmail.com>
2024-12-22 01:08:19 +0100

    run

--- main.py
+++ main.py
@@ -2,3 +2,8 @@
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     import lecture07_differential_photometry
+    import lecture08_tess_pixels_analysis
+    import lecture09_tess_light_curves
+    import lecture10_tess_light_curves_filtering
+    import lecture11_tess_light_curve_transit_model
+    import lecture12_tess_light_curve_model_fit
